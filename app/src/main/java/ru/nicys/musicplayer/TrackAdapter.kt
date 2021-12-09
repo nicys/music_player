9877303e5a7e0186eb3256f8ff4e6dd98d6c2967
nicys <nicys2476@yandex.ru>
2021-12-09 15:21:51 +0700

    added TrackAdapter.kt

---
+++ app/src/main/java/ru/nicys/musicplayer/TrackAdapter.kt
@@ -0,0 +1,70 @@
+package ru.nicys.musicplayer
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.nicys.musicplayer.databinding.TrackCardBinding
+import ru.nicys.musicplayer.dto.Track
+
+interface OnInteractionListener {
+    fun onPlay(track: Track) {}
+    fun onPause(track: Track) {}
+}
+
+class TrackAdapter(
+    private val onInteractionListener: OnInteractionListener
+) : ListAdapter<Track, TrackViewHolder>(TrackDiffCallBack()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val binding = TrackCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TrackViewHolder(binding, onInteractionListener)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bind(it)
+        }
+
+    }
+}
+
+class TrackViewHolder(
+    private val binding: TrackCardBinding,
+    private val onInteractionListener: OnInteractionListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(track: Track) {
+        binding.apply {
+            tvTrackName.text = track.file
+            if (track.isPlaying) {
+                fabPlayButton.setImageResource(R.drawable.ic_pause_48)
+            } else {
+                fabPlayButton.setImageResource(R.drawable.ic_play_48)
+            }
+
+            fabPlayButton.setOnClickListener {
+                when (track.isPlaying) {
+                    false -> {
+                        onInteractionListener.onPlay(track)
+                        track.isPlaying = true
+                    }
+                    true -> {
+                        onInteractionListener.onPause(track)
+                        track.isPlaying = false
+                    }
+                }
+                onInteractionListener.onPlay(track)
+            }
+        }
+    }
+}
+
+class TrackDiffCallBack : DiffUtil.ItemCallback<Track>() {
+    override fun areItemsTheSame(oldItem: Track, newItem: Track): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Track, newItem: Track): Boolean {
+        return oldItem == newItem
+    }
+}
