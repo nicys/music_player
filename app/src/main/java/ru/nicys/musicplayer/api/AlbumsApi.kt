b390504d29cf22205b6fa46bc16d114e9ff02709
nicys <nicys2476@yandex.ru>
2021-12-09 23:04:04 +0700

    added AlbumsApi.kt

---
+++ app/src/main/java/ru/nicys/musicplayer/api/AlbumsApi.kt
@@ -0,0 +1,25 @@
+package ru.nicys.musicplayer.api
+
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import ru.nicys.musicplayer.BuildConfig.BASE_URL
+import ru.nicys.musicplayer.dto.Album
+
+interface AlbumApi {
+    @GET("album.json")
+    suspend fun getAlbum(): Response<Album>
+
+    companion object {
+
+        private val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+
+        val apiService: AlbumApi by lazy {
+            retrofit.create(AlbumApi::class.java)
+        }
+    }
+}
