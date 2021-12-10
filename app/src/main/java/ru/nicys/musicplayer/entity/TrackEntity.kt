c7786cd9355c44c52ec02539191e203d1f37822d
nicys <nicys2476@yandex.ru>
2021-12-10 13:44:33 +0700

    added TrackEntity.kt

--- app/src/main/java/ru/nicys/musicplayer/entity/TrackEntity.kt
+++ app/src/main/java/ru/nicys/musicplayer/entity/TrackEntity.kt
@@ -1,4 +1,25 @@
 package ru.nicys.musicplayer.entity
 
-class TrackEntity {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.nicys.musicplayer.dto.Track
+
+@Entity
+data class TrackEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val file: String,
+    var isPlaying: Boolean = false,
+    val valalbumId: Int,
+) {
+    fun toDto(): Track = Track(id, file, isPlaying, valalbumId)
+
+    companion object {
+        fun fromDto(dto: Track) = with(dto) {
+            TrackEntity(id, file, isPlaying, albumId)
+        }
+    }
 }
+
+fun List<TrackEntity>.toTrack(): List<Track> = map(TrackEntity::toDto)
+fun List<Track>.toEntity(): List<TrackEntity> = map(TrackEntity::fromDto)
