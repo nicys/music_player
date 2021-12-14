77837688c92361c94f507f24e072ce2e0b953341
nicys <nicys2476@yandex.ru>
2021-12-14 11:22:33 +0700

    corrected TracksViewModel.kt

--- app/src/main/java/ru/nicys/musicplayer/viewModel/TracksViewModel.kt
+++ app/src/main/java/ru/nicys/musicplayer/viewModel/TracksViewModel.kt
@@ -3,31 +3,21 @@ package ru.nicys.musicplayer.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.launch
 import ru.nicys.musicplayer.db.AppDb
 import ru.nicys.musicplayer.dto.Album
-import ru.nicys.musicplayer.dto.Track
 import ru.nicys.musicplayer.repository.TrackRepository
 import ru.nicys.musicplayer.repository.TrackRepositoryImpl
 import ru.nicys.musicplayer.utils.SingleLiveEvent
-
-private var emptyAlbum = Album(0, "", "", "", "", "", emptyList())
-private var emptyTrack = Track(0, "", false, 0)
 
 class TracksViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: TrackRepository = TrackRepositoryImpl(
         AppDb.getInstance(context = application).trackDao(),
     )
 
-    val dataTracks = repository.data
-
-    private val _tracks = MutableLiveData(emptyTrack)
-    val tracks: LiveData<Track>
-        get() = _tracks
+    val dataTracks = repository.data.asLiveData()
 
     private val _loadTracksExceptionEvent = SingleLiveEvent<Unit>()
     val loadTracksExceptionEvent: LiveData<Unit>
@@ -39,16 +29,4 @@ class TracksViewModel(application: Application) : AndroidViewModel(application) 
                 e.printStackTrace()
                 _loadTracksExceptionEvent.call()
             }
-
-    private val _album = MutableLiveData(emptyAlbum)
-    val album: LiveData<Album>
-        get() = _album
-
-    fun isPlaying(id: Int) = viewModelScope.launch {
-        try {
-            repository.isPlaying(id)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
 }
